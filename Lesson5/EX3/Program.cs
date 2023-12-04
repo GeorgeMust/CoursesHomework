63a96ba4a745cd7b26d5c866fd5c3711594a5776
George <yuramustik@gmail.com>
2023-12-04 16:12:04 +0300

    Factorial

---
+++ Lesson5/EX3/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace EX3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите факториал: ");
+            Console.WriteLine(Factorial(InpStr()));
+        }
+        static int InpStr()
+        {
+            bool parseOk = false;
+            while (!parseOk)
+            {
+                string? strIn = Console.ReadLine();
+                if (int.TryParse(strIn, out int num))
+                {
+                    return num;
+                }
+                else
+                {
+                    Console.WriteLine("Введён неверный формат данных");
+                }
+            }
+            return 0;
+        }
+        static int Factorial(int num)
+        {
+            if (num == 0)
+            {
+                return 1;
+            }
+            else if (num == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                int n1 = Factorial(num - 1);
+                return (n1 * num);
+            }
+        }
+    }
+}
