3b6ebe89787f3a64500772ae94bca9c8424ced27
George <yuramustik@gmail.com>
2023-12-11 13:15:32 +0300

    invert massive

---
+++ Lesson7/ClassLibrary1/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml.Linq;
+
+namespace ClassLibrary1
+{
+    public class ArrayWorker
+    {
+        string[] InpArr;
+        public ArrayWorker(string[] InpArr)
+        {
+            this.InpArr = InpArr;
+        }
+        public string[] InversArr(string[] InpArr)
+        {
+            for (int i = InpArr.Length - 1, j = 0; j < i; i--, j++)
+            {
+                string forTime = InpArr[i];
+                InpArr[i] = InpArr[j];
+                InpArr[j] = forTime;
+            }
+            return InpArr;
+        }
+        public void Print()
+        {
+            for (int i = 0; i < InpArr.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}-й элемент массива: {InpArr[i]}");
+            }
+        }
+    }
+}
---
+++ Lesson7/L7Ex1/Program.cs
@@ -0,0 +1,18 @@
+﻿using ClassLibrary1;
+
+namespace Les7Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Создайте ClassLibrary с типом ArrayWorker, который будет уметь принимать массив, инвертировать его и возвращать.
+             * Массив передаем сами явно в метод, а не через консоль.*/
+
+            string[] firstArr = { "abs", "abt", "prt" };
+            ArrayWorker arrayWorker = new ArrayWorker(firstArr);
+            arrayWorker.InversArr(firstArr);
+            arrayWorker.Print();
+        }
+    }
+}
