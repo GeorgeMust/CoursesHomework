2080d45f68247c8608d719f6a6eb13b370cb7230
George <yuramustik@gmail.com>
2023-12-11 13:19:13 +0300

    cone

---
+++ Lesson7/ClassLibrary2/Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace ClassLibrary2
+{
+    public class Cone
+    {
+        public const double pi = Math.PI;
+
+        public double Radius { get; set; }
+        public double Height { get; set; }
+        public Cone(
+            double radius,
+            double height)
+        {
+            Radius = radius;
+            Height = height;
+        }
+        public double CalcSground()
+        {
+            return pi * Radius * Radius;
+        }
+        public double CalcSfull()
+        {
+            return pi * Radius * (Radius + Math.Sqrt(Radius * Radius + Height * Height));
+        }
+    }
+}
---
+++ Lesson7/L7Ex2/Program.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary2;
+
+namespace Zadan2Les7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Создайте ClassLibrary с типом Cone.Конструктор типа принимает 2 параметра: радиус(r) и высоту(h). 
+             * Так же в типе есть два метода, которые рассчитывают площадь поверхности основания и полную площадь.*/
+
+            Cone cone = new Cone(3, 4);
+            Console.WriteLine("Площадь основания конуса: " + cone.CalcSground());
+            Console.WriteLine("Полная площадь конуса: " + cone.CalcSfull());
+        }
+    }
+}
