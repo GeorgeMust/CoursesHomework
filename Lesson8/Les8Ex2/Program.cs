dcbd7cc98a7c5166dfd54337ecec7d45fd146965
George <yuramustik@gmail.com>
2023-12-13 14:49:01 +0300

    Abstract Class

---
+++ Lesson8/Les8Ex2/Program.cs
@@ -0,0 +1,50 @@
+﻿/*Реализовать структуру классов : Engine(содержит метод для включения двигателя), Transport(абстрактный класс), Car, Plane, абстрактные члены: 
+свойство типа Engine 
+метод Move. 
+Переопределение Move должно вызывать приватную логику движения наследников и включать двигатель.*/
+using System.Buffers;
+
+namespace Les8Ex2
+{
+    abstract class Transport
+    {        
+        public abstract void Move();
+        public Engine engine { get; set; } = new Engine();
+    }
+    public class Engine
+    {
+        private string onAct = "Двигатель выключен";
+        public void OnEng()
+        {
+            this.onAct = "Двигатель включен";
+            Console.WriteLine(this.onAct);
+        }
+    }
+    internal class Car: Transport
+    {
+        public override void Move()
+        {
+            engine.OnEng();
+            Console.WriteLine("Машина едет");            
+        }
+    }
+    internal class Plane: Transport
+    {
+        public override void Move()
+        {
+            engine.OnEng();
+           Console.WriteLine("Самолёт летит");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+        //    car.engine = new Engine();
+            car.Move();
+            Plane plane = new Plane();
+            plane.Move();
+         }
+    }
+}
