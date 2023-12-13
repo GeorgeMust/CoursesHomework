7de270a9cc415044991c2aa06a0950a4715dcebb
George <yuramustik@gmail.com>
2023-12-13 17:38:46 +0300

    Extended the method

---
+++ Lesson8/Les8Ex3/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Создайте метод расширения для массива int, который возвращает наибольший элемент.*/
+namespace Les8Ex3
+{    public static class IntMaxNumber
+    {
+        public static int IntMaxNum(this int[] intArr)
+        {
+            return intArr.Max();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArr = { 1, 6, 3, 4, };
+            Console.WriteLine(intArr.IntMaxNum());
+        }
+
+    }
+}
