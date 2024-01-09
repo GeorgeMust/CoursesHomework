272ae26fd51cb0bbd849660579b2a9a9a547d55f
George <yuramustik@gmail.com>
2024-01-09 17:27:16 +0300

    Generik lesson12Ex5

---
+++ Lesson12/Lesson12Ex5/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Создать тип MobileNetwork и дженерик тип MobilePhone<T> который имеет свойство типа T с названием Network. Добавить ограничение что T является MobileNetwork или его наследником.*/
+namespace Lesson12Ex5
+{
+    public class MobileNetwork
+    {
+
+    }
+
+    public class MobilePhone<T> where T : MobileNetwork
+    {
+        public T Network { get; set; }
+    }
+
+    public class SpecificMobileNetwork : MobileNetwork
+    {
+
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MobilePhone<SpecificMobileNetwork> phone = new MobilePhone<SpecificMobileNetwork>();
+            phone.Network = new SpecificMobileNetwork();            
+        }
+    }
+}
