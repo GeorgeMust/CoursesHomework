3c4effaea26692cc156f86ac4b3da58cd0e67e02
George <yuramustik@gmail.com>
2024-01-23 16:27:41 +0300

    lesson16 ex1 linq

---
+++ Lesson16/Lesson16Ex1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*1.Дана целочисленная последовательность [-10, 22, 13, 43, -5, -12, 100].
+Извлечь из нее все четные отрицательные числа, поменяв порядок извлеченных чисел на обратный.
+*/
+using System.Linq;
+namespace Lesson16Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mass = new int[] { -10, 22, 13, 43, -5, -12, 100 };
+            var rezult = mass.Where(n => n <0 && n%2 == 0).Reverse();
+            
+            foreach (int n in rezult)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
