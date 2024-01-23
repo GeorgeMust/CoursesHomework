3ec0af9f6ba4615ad47629e2e48f3826aaea8388
George <yuramustik@gmail.com>
2024-01-23 17:05:17 +0300

    Less16Ex2 linq

---
+++ Lesson16/Lesson16_2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*2.Дана последовательность непустых строк A . ["Hello", "here", "we", "are"]. Получить последовательность символов, каждый элемент которой является начальным символом соответствующей строки
+из A. Порядок символов должен быть обратным по отношению к порядку элементов исходной последовательности.*/
+namespace Lesson16_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] posled = new string[] { "Hello", "here", "we", "are" };
+            var rezultPosled = from n in posled
+                               select n.First();
+            rezultPosled = rezultPosled.Reverse();
+            foreach (var n in rezultPosled) { Console.WriteLine(n); }
+        }
+    }
+}
