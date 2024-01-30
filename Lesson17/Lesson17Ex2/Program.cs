4d106b2f5ae1a2556b83f425f1f86dd10c87f9d9
George <yuramustik@gmail.com>
2024-01-30 18:33:48 +0300

    Lesson17 Ex2

---
+++ Lesson17/Lesson17Ex2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string directoryPath = "C:\\Work\\КурсыC#";
+        string[] folderNames = new string[20];
+
+        // Добавил создание директорий
+        for (int i = 0; i < 20; i++)
+        {
+            string folderName = "MyTestFolder" + i;
+            string folderPath = Path.Combine(directoryPath, folderName);
+            Directory.CreateDirectory(folderPath);
+            folderNames[i] = folderPath;
+        }
+
+        foreach (string folderPath in folderNames)
+        {
+            if (Directory.Exists(folderPath))
+            {
+                Directory.Delete(folderPath);
+                Console.WriteLine("Папка удалена: " + folderPath);
+            }
+        }
+    }
+}
