dce6fd43ff3c9a6fdea7da1e5835dc2b23068e39
George <yuramustik@gmail.com>
2024-01-30 18:36:01 +0300

    Lesson17 Ex3

---
+++ Lesson17/Lesson17Ex3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string filePath = "C:\\Work\\КурсыC#\\example.txt"; // Путь к файлу
+
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            writer.WriteLine("Привет с первой строки");
+            writer.WriteLine();
+            writer.WriteLine("Привет с 3й строки");
+        }
+
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
