1807eaa7abf629adb31a6f21809e4ae250eb62e8
George <yuramustik@gmail.com>
2024-02-01 18:39:50 +0300

    Lesson18Ex3

---
+++ Lesson18/Lesson18Ex3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+        Parallel.ForEach(numbers, number =>
+        {
+            int square = number * number;
+            Console.WriteLine("Квадрат числа {0} равен {1}", number, square);
+        });
+
+        Console.ReadLine();
+    }
+}
