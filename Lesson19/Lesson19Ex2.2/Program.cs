afef2bc430614a40bd46efa53b370125721a1b7c
George <yuramustik@gmail.com>
2024-02-04 19:18:34 +0300

    Lesson19 Ex2

---
+++ Lesson19/Lesson19Ex2.2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Lesson19Ex2._2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int time = 2000;
+            RunTaskWithAwaiter(time).GetAwaiter().GetResult();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        public static Task RunTaskWithAwaiter(int time)
+        {
+            return Task.Delay(time).ContinueWith(_ => Console.WriteLine("Hello from callback"));
+        }
+    }
+}
---
+++ Lesson19/Lesson19Ex2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*2.Создать метод принимающий величину милисекунд time,и запускающий задачу которая через определенное время(равное параметру time) выполняет вывод в консоль
+строки "Hello from callback" посредством продолжения. Попробовать сделать это через awaiter и через ContinueWith*/
+using System;
+using System.Threading.Tasks;
+namespace Lesson19Ex2
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            int time = 2000;
+            await RunCallbackAfterDelay(time);            
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+        public static async Task RunCallbackAfterDelay(int time)
+        {
+            await Task.Delay(time);
+            Console.WriteLine("Hello from callback");
+        }
+    }
+}
